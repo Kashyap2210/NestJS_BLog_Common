ba20b7bdf601fe47089ef4750d97a1baed8ca3de
Kashyap 2210 <kash.cdac@gmail.com>
2025-01-05 16:23:45 +0530

    blog create dto author name remove

--- dist/interfaces/dtos/blog.create.dto.interface.d.ts
+++ dist/interfaces/dtos/blog.create.dto.interface.d.ts
@@ -2,6 +2,5 @@ export interface IBlogCreateDto {
     title: string;
     keywords: string;
     content: string;
-    author: string;
 }
 export type IBulkBlogCreateDto = [IBlogCreateDto];
--- src/interfaces/dtos/blog.create.dto.interface.ts
+++ src/interfaces/dtos/blog.create.dto.interface.ts
@@ -2,7 +2,6 @@ export interface IBlogCreateDto {
   title: string;
   keywords: string;
   content: string;
-  author: string;
 }
 
 export type IBulkBlogCreateDto = [IBlogCreateDto];
