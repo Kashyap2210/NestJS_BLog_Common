5c7313433c21f611fd4894349e5f3c7726037f06
Kashyap Solanki <kash.cdac@gmail.com>
2025-02-03 19:21:22 +0530

    blog response update

--- dist/interfaces/response/blog.response.interface.d.ts
+++ dist/interfaces/response/blog.response.interface.d.ts
@@ -1,6 +1,8 @@
 import { IBlogEntity } from '../entities/blog.entity.interface';
+import { IBlogLikesCounterEntity } from '../entities/blog.likes.counter.entity.interface';
 import { ICommentEntity } from '../entities/comment.entity.interface';
 export type IBlogResponse = {
     blog: IBlogEntity;
     comments: ICommentEntity[];
+    likes: IBlogLikesCounterEntity[];
 };
--- src/interfaces/response/blog.response.interface.ts
+++ src/interfaces/response/blog.response.interface.ts
@@ -1,7 +1,9 @@
 import { IBlogEntity } from '../entities/blog.entity.interface';
+import { IBlogLikesCounterEntity } from '../entities/blog.likes.counter.entity.interface';
 import { ICommentEntity } from '../entities/comment.entity.interface';
 
 export type IBlogResponse = {
   blog: IBlogEntity;
   comments: ICommentEntity[];
+  likes: IBlogLikesCounterEntity[]
 };
